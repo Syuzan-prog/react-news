4c5abd8011314d1e45d9380d4d3efe1e5503bc13
Syuzan <syuzanna.ant@gmail.com>
2020-11-19 00:25:42 +0400

    Add News

---
+++ src/component/article/Article.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types'; 
+import './Article.css';
+
+class Article extends React.Component{
+    state ={
+        visible:false
+    }
+   
+    handleReadMoreClck = (e) => { 
+        this.setState({
+                visible: true 
+            })      
+        }
+        
+    render(){
+        const { author , text , bigText } = this.props.data;
+        const { visible } = this.state;
+        return(
+            <div className='article'>
+                <p className='news__author '>{author}</p>
+                <p className='news__text'>{text}</p>
+                {
+                    !visible && <a href='#' onClick = { this.handleReadMoreClck } className='new
+                    s__readmore'>Подробнее</a>
+                }
+                {
+                    visible && <p>{bigText}</p>
+                }
+                
+            </div>
+        )
+    }
+    
+}
+
+Article.propTypes = {
+    data: PropTypes.shape({
+        id: PropTypes.number.isRequired, 
+        author: PropTypes.string.isRequired,
+        text: PropTypes.string.isRequired,
+        bigText: PropTypes.string.isRequired
+    })
+}
+    
+
+export  {Article};
